b153a4028b0bdd0b4f5a356288163956831b5dda
anamkravanja <anamarija.kravanja@gmail.com>
2018-10-27 11:29:47 +0200

    zaključila

--- 2._vaje/Kravanja2.R
+++ 2._vaje/Kravanja2.R
@@ -1,10 +1,11 @@
 library(actuar)
 
+#KOMULATIVNI MODEL TVEGANJA IN PANGERJEV ALGORITEM
+
 # 1.)
 #a.)
 
 vzorec <- read.delim("vzorec2.txt",header = FALSE)
-View(vzorec)
 
 hist(vzorec$V1, main="histogram",
      xlab="vzorec",ylab= "pogostost pojavljanja",
@@ -43,7 +44,7 @@ curve(pweibull(x, shape=k, scale = lambda),
       col = "red",
       lwd = 2) #komulativna je pweibull
 
-#d.) 
+#d.) (porazdelitev števila odškodninskih zahtevkov je porazdeljena binomsko)
 n <-20
 p <- 1/2
 upanjey <- lambda * gamma(1 + (1/k))
@@ -55,7 +56,7 @@ variancan <- n*p* (1-p)
 upanjeyna2 <- varianca_y + upanjey^2
 variancaS <- variancay * upanjen + variancan * upanjeyna2
 
-# 2.)
+# 2.) PANJERJEV ALGORITEM
 # a.)
 h <- 0.25
 n <- 38
@@ -83,18 +84,38 @@ legend("topright", legend=c('diskretizacija', 'Weibullova porazdelitev'), col=c(
 FS <- aggregateDist(method = 'recursive',
                     model.freq = 'poisson',
                     model.sev = diskretno,
-                    lambda = lambda,
+                    lambda = 10,
                     x.scale = 0.25,
                     maxit = 1000000,
-                    tol = 0.005)
+                    tol = 0.025)
 plot(FS,
      main = 'Porazdelitvena funkcija odškodnin',
      xlab = 'Višina odškodnine')
 
-#d.) 
+#d.) upanje in disperzija komulativne škode
 upanjedrugoS <- mean(FS)
 variancadrugaS <- sum(knots(FS)^2 * diff(FS)) - upanjedrugoS^2
 
 
-#3.) Metoda Monte Carlo
-#a.)
+#3.) METODA MONTE CARLO
+#a.) 
+simulacijaN <- rpois(10000,10) #simuliranje spr. N, rpois generira nakljuèno s pomoèjo Poissiona
+simulacijaS <- c(0) #simuliranje neodvisnih realizacije spremenljivke Y
+for (i in 1:10000){
+  simulacijaS[i] <- sum(rweibull(simulacijaN[i], k, lambda))
+}
+
+#b.) ocena upanja in disperzije iz simulacije
+upanjetretjeS <- mean(simulacijaS)
+variancatretjaS<- var(simulacijaS)
+
+#c.) slika simulirane porazdelitvene funkcije in primerjava s porazdelitvijo iz 2c
+plot(ecdf(simulacijaS),
+     col = 'green',
+     add = TRUE )
+
+legend('bottomright', 
+       legend = c('Panjerjev algoritem', 'Monte Carlo simulacija'),
+       col = c('black', 'green'),
+       lty=1:1, cex=0.9)
+
